5eaa309444d6d83d238990eab5b0f71175c97b52
priyambi <79246912+priyambi@users.noreply.github.com>
2021-07-29 16:33:15 +0530

    Update main.py

--- BACKENDTASK1/main.py
+++ BACKENDTASK1/main.py
@@ -1,11 +1,11 @@
-from collections import Counter
+from collections import Counter         #sub-class inside dictionary class
 
 n = int(input("Enter a number: "))
 print(n)
 lst = []
 for i in range(0, n):
     x = str(input())
-    lst.append(x)
+    lst.append(x)                      #adding each string(total n strings) to the list
 x = Counter(lst)
-print(len(x))
-print(Counter(lst))
+print(len(x))                          # prints unique number of words
+print(Counter(lst))                    # prints the frequency of each word
